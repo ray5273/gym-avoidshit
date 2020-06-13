fdba7c808074d0af00f217b0f56f6834f5162900
sanghyeok <ray5273@naver.com>
2020-06-13 21:56:37 +0900

    [ADD] Added gym-avoidshit version 1.0 ,discrete

---
+++ gym_avoidshit/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_avoidshit.envs.avoidshit_env import *
---
+++ test.py
@@ -0,0 +1,18 @@
+import gym
+import gym_avoidshit
+import random
+env = gym.make('AvoidShit-v0')
+
+def get_action():
+    return random.randint(-1,1)
+
+
+for i in range(1000):
+    done = False
+    env.reset()
+    env.render()
+    while not done:
+        action = get_action()
+        state,reward,done,_ = env.step(action)
+        env.render()
+
